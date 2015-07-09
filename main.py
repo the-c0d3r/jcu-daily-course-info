7e096ac5bd989ebe09aa4f61690ad94d1dcf2968
the-c0d3r <thuyeintun@my.jcu.edu.au>
2015-07-09 16:28:54 +0800

    Fully working program now, with 24 hour time to 12 hour time conversion

--- main.py
+++ main.py
@@ -4,11 +4,48 @@ import sys
 import datetime
 import os
 
-coursekey = ['LS0300', 'BU1805']
+coursekey = [i for i in open('course.txt').read().split('\n') if i != '']
+# Reads the course.txt file and add it to coursekey if the data is not empty
+
+banner1 = """
+   $$$$$\  $$$$$$\  $$\   $$\ 
+   \__$$ |$$  __$$\ $$ |  $$ |
+      $$ |$$ /  \__|$$ |  $$ |
+      $$ |$$ |      $$ |  $$ |
+$$\   $$ |$$ |      $$ |  $$ |
+$$ |  $$ |$$ |  $$\ $$ |  $$ |
+\$$$$$$  |\$$$$$$  |\$$$$$$  |
+ \______/  \______/  \______/ """
+
+banner2 = """
+    .S    sSSs   .S       S.   
+   .SS   d%%SP  .SS       SS.  
+   S%S  d%S'    S%S       S%S  
+   S%S  S%S     S%S       S%S  
+   S&S  S&S     S&S       S&S  
+   S&S  S&S     S&S       S&S  
+   S&S  S&S     S&S       S&S  
+   S&S  S&S     S&S       S&S  
+   d*S  S*b     S*b       d*S  
+  .S*S  S*S.    S*S.     .S*S  
+sdSSS    SSSbs   SSSbs_sdSSS   
+YSSY      YSSP    YSSP~YSSY    
+"""
+
+# Website used to generate banner => http://patorjk.com/software/taag/
+# Banner1 font = Big Money-nw
+# Banner2 font = AMC AAA01
+
+display_banner = True
+# Replace "True" with "False" if you want to disable banner
 
 class course():
 
     def __init__(self):
+        if display_banner:
+            print banner2
+            # Options, banner1 or banner2, whichever banner you like
+
         self.page = self.getpage()
         self.parse(self.page)
 
@@ -31,61 +68,47 @@ class course():
         Gets the input as list
         Returns the course info
         """
-        print("[~] Parsing the Data")
-        course_info = []
-        filtered = []
+        course_info = [] # to keep the course data inside as list which is stated by coursekey
+
+        filtered = [] # to keep all the subject list
+
         pat = re.compile(r'<td class="BTsubj">(.+)</td><td class="BTclass">(.+)</td><td class="BTtime">(.+)</td><td class="BTroom">(.+)</td></tr>')
 
         for i in data:
+        # Filter all the html code starting with <td class="BTsubj"
             if len(pat.findall(i)) != 0:
+            # If the pattern is matched
                 filtered.append(pat.findall(i))
-            else:
-                pass
+                # The variable "i" is added to filtered list
+            else: pass
 
         for i in filtered:
             for a in coursekey:
                 if a in i[0][0]:
-                    print "Course : {}\nType : {}\nTime : {}\nRoom : {}".format(i[0][0],i[0][1],i[0][2],i[0][3])
+                    duration = self.convert_time(i[0][2][:5])+" ~ "+self.convert_time(i[0][2][8:])
+                    # i[0][2][:5] is the start time and i[0][2][8:] is end time
+                    # get those time strings and call convert_time to convert them into 12 hour format.
+                    print("\nCourse : {}\nType : {}\nTime : {}\nRoom : {}\n".format(i[0][0],i[0][1],duration,i[0][3]))
+                    # Printing the course information.
+
+
+        # Example response from Regular Expression, just for debugging purpose
         #[('BU1805 - Contemporary Business Communications', 'LA,B,C,D,E,F', '16:00 - 17:50', 'C4-14')]
-        #print "BU1805" in filtered[5][0][0]
 
-
-# 
     def convert_time(self,data):
         """
         Get input as string
         Convert it into 12 hour format if necessary
+        And return it as string
         """
 
-        hour = data[:data.index(':')]
+        hour = int(data[:data.index(':')])
         minutes = data[data.index(':')+1:]
 
         if hour > 12:
             hour -= 12
 
-        return str(hour,':',minutes)
+        return str(hour)+':'+str(minutes)
 
 if __name__ == '__main__':
     app = course()
-
-
-"""
-Check if the subject code is empty
-    YES : Ask for it | NO : Pass
-Check if the html file exists and date
-    If exist and date inconsistent:
-        get new page and save file
-    Else:
-        pass
-
-
-Flow
-1. Get file
-2. Parse the file, get course information 
-3. Print them
-
-Modules
-
-class course -> INIT -> getpage -> exists -> 
-
-"""
